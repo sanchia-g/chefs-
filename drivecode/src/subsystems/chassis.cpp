cb7ffdc4cab3c759d49e9d70febcb8bfbc387928
InfinityChicken <8zsc2014@gmail.com>
2024-01-27 18:31:43 -0800

    Update chassis.cpp
    Changed variable names and values

--- drivecode/src/subsystems/chassis.cpp
+++ drivecode/src/subsystems/chassis.cpp
@@ -2,14 +2,14 @@
 using namespace okapi::literals;
 
 //ARBITRARY PORTS!!!!
-const int8_t left1 = 1; 
-const int8_t left2 = -2; //neg bc the middle motor is reversed
-const int8_t left3 = 3;
-const int8_t right1 = -4;
-const int8_t right2 = 5; //pos bc the middle motor is reversed
-const int8_t right3 = -6;
+const int8_t leftBack = 1; 
+const int8_t leftMid = 2; 
+const int8_t leftFront = 3;
+const int8_t rightBack = -4;
+const int8_t rightMid = -5; 
+const int8_t rightFront = -6;
 
 std::shared_ptr<okapi::ChassisController> chassis = okapi::ChassisControllerBuilder()
-    .withMotors({left1, left2, left3}, {right1, right2, right3})
+    .withMotors({leftBack, leftMid, leftFront}, {rightBack, rightMid, rightFront})
     .withDimensions(okapi::AbstractMotor::gearset::blue, {{3.25_in, }, okapi::imev5BlueTPR}) //TODO: get wheel track
     .build();
