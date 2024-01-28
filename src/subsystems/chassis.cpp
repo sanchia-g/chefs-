ae0f76291b2ab63a5dcbf789c64db5871f277062
Archer2305 <73808514+Archer2305@users.noreply.github.com>
2024-01-28 13:10:17 -0800

    Update chassis.cpp

--- src/subsystems/chassis.cpp
+++ src/subsystems/chassis.cpp
@@ -8,8 +8,16 @@ const int8_t leftFront = 3;
 const int8_t rightBack = -4;
 const int8_t rightMid = -5; 
 const int8_t rightFront = -6;
+//these are just motor port vales, you never initialised actual motors
 
 std::shared_ptr<okapi::ChassisController> chassis = okapi::ChassisControllerBuilder()
+//make this odom       ^                                    ^
     .withMotors({leftBack, leftMid, leftFront}, {rightBack, rightMid, rightFront})
+//                ^ cannot construct a motor group with integer values
     .withDimensions(okapi::AbstractMotor::gearset::blue, {{3.25_in, }, okapi::imev5BlueTPR}) //TODO: get wheel track
+//are you using blue cartridges?                    ^                                ^
     .build();
+//build with odom!!!
+
+
+//where yo updateChassis function
