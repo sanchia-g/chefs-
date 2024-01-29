27337108187fc3ee05024dc7161c1df1e74e3db3
InfinityChicken <8zsc2014@gmail.com>
2024-01-28 20:19:07 -0800

    Update chassis.hpp

--- include/subsystems/chassis.hpp
+++ include/subsystems/chassis.hpp
@@ -1,9 +1,8 @@
 #include "main.h"
-extern const int8_t left1; //motor on the bottom of the left stack 
-extern const int8_t left2; //motor in the middle of the left stack
-extern const int8_t left3; //motor on the top of the left stack
-extern const int8_t right1; //motor on the bottom of the right stack
-extern const int8_t right2; //motor in the middle of the right stack
-extern const int8_t right3; //motor on the top of the right stack
-//chat where yo motors at; the things above are just integers
+extern const int8_t leftBack;
+extern const int8_t leftMid; 
+extern const int8_t leftFront; 
+extern const int8_t rightBack;
+extern const int8_t rightMid;
+extern const int8_t rightFront;
 extern std::shared_ptr<okapi::ChassisController> chassis;
